14ad2cb3b317d3288718388cd989c5d9e6b1d8b6
Yusuf Gündüz <Kambaa@users.noreply.github.com>
2018-08-02 16:22:45 +0300

    Update publish.php

--- examples/publish.php
+++ examples/publish.php
@@ -2,13 +2,13 @@
 
 require("../phpMQTT.php");
 
-$server = "mqtt.example.com";     // change if necessary
-$port = 1883;                     // change if necessary
+$server = "127.0.0.1";     // change if necessary
+$port = 2222;                     // change if necessary
 $username = "";                   // set your username
 $password = "";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
-$mqtt = new phpMQTT($server, $port, $client_id);
+$mqtt = new \Bluerhinos\phpMQTT($server, $port, $client_id);
 
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
