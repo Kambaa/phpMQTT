fb20fc96319fa028c99b6a7ed6e52e6e8c309b8b
Yusuf Gündüz <Kambaa@users.noreply.github.com>
2018-08-02 16:23:08 +0300

    Update subscribe.php

--- examples/subscribe.php
+++ examples/subscribe.php
@@ -3,13 +3,13 @@
 require("../phpMQTT.php");
 
 
-$server = "mqtt.example.com";     // change if necessary
-$port = 1883;                     // change if necessary
+$server = "127.0.0.1";     // change if necessary
+$port = 2222;                     // change if necessary
 $username = "";                   // set your username
 $password = "";                   // set your password
 $client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
 
-$mqtt = new phpMQTT($server, $port, $client_id);
+$mqtt = new \Bluerhinos\phpMQTT($server, $port, $client_id);
 
 if(!$mqtt->connect(true, NULL, $username, $password)) {
 	exit(1);
